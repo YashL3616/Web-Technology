7fec4be866729b7a0898b679039912841755032f
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-02-28 19:35:09 +0530

    Add files via upload

---
+++ Java/whileeg.java
@@ -0,0 +1,9 @@
+class whileeg{
+    public static void main(String [] args) {
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("hello");
+            count++;
+        }
+    }
+}
