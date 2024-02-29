959c78b25408b28e61407a92b47189def82aa3f0
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-02-29 07:52:38 +0530

    Add files via upload

---
+++ Java/Ifelse.java
@@ -0,0 +1,14 @@
+class ifelse {
+    public static void main(String [] args) {
+        int a = 25, b = 20, c = 30;
+        int big;
+        if (a > b && a > c) {
+            big = a;
+        } else if (b > c) {
+            big = b;
+        } else {
+            big = c;
+        }
+        System.out.println(big);
+    }
+}
